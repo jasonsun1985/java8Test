c23f32b27bf857e2b087cb33ecaca1c5331e739a
jasonsun1985 <jasonsun1985@sohu.com>
2020-03-05 09:56:17 +0800

    update

--- src/main/java/com/tec/test/Test8.java
+++ src/main/java/com/tec/test/Test8.java
@@ -2,14 +2,15 @@ package com.tec.test;
 
 import static java.util.stream.Collectors.toList;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -18,55 +19,108 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class Test8 {
-	public static void main(String[] args) {
-		testLambda();
-		testisPresent();
-		testDistinct();
-		testFilter();
-		testMap();
-		testLimit();
-		testSkip();
-		testComprehensive();
-		testFlatMap();
-		testPredicate();
-		testReduce();
-		testFunction();
-	}
-	private static void testFunction() {
-	    BiFunction<String,String,Integer> biFun=(s1,s2)->s1.length()+s2.length();
+    public static void main(String[] args) {
+        testLambda();
+        testisPresent();
+        testDistinct();
+        testFilter();
+        testMap();
+        testLimit();
+        testSkip();
+        testComprehensive();
+        testFlatMap();
+        testPredicate();
+        testReduce();
+        testFunction();
+        testConverter();
+        testColon();
+        testSupplier();
+        testInstant();
+
+    }
+
+    private static void testInstant() {
+        Instant instant = Instant.now();
+        System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:SS").format(LocalDateTime.now()));
+        System.out.println("|||||||||||||||||||||||||||||||||testInstant||||||||||||||||||||||||||||");
+
+    }
+
+    private static void testSupplier() {
+        Supplier<People> personSupplier = People::new;
+        personSupplier.get();
+    }
+
+    private static void testColon() {
+        List<String> list = Arrays.asList("a", "b", "c", "d");
+        Consumer<String> methodParam = Test8::printValue;
+        list.forEach(methodParam::accept);
+        String s1 = "1";
+        String s2 = "2";
+
+        //JDK8中有双冒号的用法，就是把方法当做参数传到stream内部，使stream的每个元素都传入到该方法里面执行一下
+        list.forEach(Test8::printValue);
+        System.out.println("|||||||||||||||||||||||||||||||||testColon||||||||||||||||||||||||||||");
+
+
+    }
+
+    private static void testConverter() {
+        Something something = new Something();
+        Converter<String, String> test = something::startsWith;
+        System.out.println(test.covert("JAVA"));
+        System.out.println("|||||||||||||||||||||||||||||||||testConverter||||||||||||||||||||||||||||");
+
+    }
+
+    private static void testFunction() {
+        BiFunction<String, String, Integer> biFun = getStringStringIntegerBiFunction();
+        biFun.apply("a", "b");
         Function<Integer, String> converter = (i) -> Integer.toString(i);
         Function<Integer, String> converter1 = i -> Integer.toString(i);
-        Function<Integer,Double> area = (r)->{return Math.pow(r, 2)*Math.PI;};
+        Function<Integer, Double> area = (r) -> {
+            return Math.pow(r, 2) * Math.PI;
+        };
         System.out.println(area.apply(10));
-		
-	}
-	private static void testReduce() {
-		int result1 = Stream.of(1,2,3,4,5).reduce(2, (x,y)->x*y);
-		System.out.println(result1);
-		Optional<Integer> result2 = Stream.of(1,2,3,4,5).reduce((x,y)->x*y);
-		System.out.println(result2.orElse(-1));
-		
-	}
+        System.out.println("|||||||||||||||||||||||||||||||||testFunction||||||||||||||||||||||||||||");
+    }
 
-	private static void testPredicate() {
-		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,67,87,123,21,32,99);
-		List<Integer> newList = getNewList(list,Test8::cal);
-		System.out.println(newList);
-	}
+    private static BiFunction<String, String, Integer> getStringStringIntegerBiFunction() {
+        return (s1, s2) -> s1.length() + s2.length();
+    }
 
-	private static List<Integer> getNewList(List<Integer> list, Predicate<Integer> num) {
-		return list.stream()
-				.filter(num)
-				.collect(toList());
-	}
+    private static void testReduce() {
+        int result1 = Stream.of(1, 2, 3, 4, 5).reduce(2, (x, y) -> x * y);
+        System.out.println("result1 : " + result1);
+        Optional<Integer> result2 = Stream.of(1, 2, 3, 4, 5).reduce((x, y) -> x * y);
+        System.out.println("result2 : " + result2.orElse(-1));
+        System.out.println("|||||||||||||||||||||||||||||||||testReduce||||||||||||||||||||||||||||");
+    }
 
-	private static boolean cal(Integer num) {
-		return num % 3 == 0;
-	}
+    private static void testPredicate() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 67, 87, 123, 21, 32, 99);
+        List<Integer> newList = getNewList(list, Test8::cal);
+        System.out.println(newList);
+    }
 
-	private static void testFlatMap() {
-		List<String> words = Arrays.asList("hello world", "hello java", "hello hello");
-		List<String> wordList = Arrays.asList("aa", "bb", "cc");
+    private static List<Integer> getNewList(List<Integer> list, Predicate<Integer> num) {
+        return list.stream()
+                .filter(num)
+                .collect(toList());
+    }
+
+    private static boolean cal(Integer num) {
+        return num % 3 == 0;
+    }
+
+    private static void testFlatMap() {
+        List<String> words = Arrays.asList("hello world", "hello java", "hello hello");
+        words.stream().flatMap(w -> {
+            return Stream.of(w + " copy");
+        }).collect(Collectors.toList());
+        words.stream().peek(w -> System.out.println("peek : " + w)).collect(Collectors.toList());
+
+        List<String> wordList = Arrays.asList("aa", "bb", "cc");
 		/*words.stream()
 				// 对words的每一项都做切割操作，把每一个字符串都转换为数组
 				// 执行完后数据结构如下{["hello","world"],["hello","java"],["hello","hello"]}
@@ -77,97 +131,119 @@ public class Test8 {
 						System.out.println(item);
 					}
 				});*/
-		words.stream()
-				// 把每一项转化为数组包含列表的Stream流
-				// 然后这个函数把所有的List里面的字符串都取出来放在了一个集合中，这个集合做下一次执行的数据源
-				// {"hello","world","hello","java","hello","hello"}
-				.flatMap(item -> {
-					String[] arr = item.split(" ");
-					return Arrays.asList(arr).stream();
-				})
-				.collect(toList())
-				// 根据每一项的HashCode和equals方法做去重操作
+        words.stream()
+                // 把每一项转化为数组包含列表的Stream流
+                // 然后这个函数把所有的List里面的字符串都取出来放在了一个集合中，这个集合做下一次执行的数据源
+                // {"hello","world","hello","java","hello","hello"}
+                .flatMap(item -> {
+                    String[] arr = item.split(" ");
+                    return Arrays.asList(arr).stream();
+                })
+                .collect(toList())
+                // 根据每一项的HashCode和equals方法做去重操作
 //				.distinct()
-				// 打印每一项
-				.forEach(item -> System.out.println(item));
-		System.out.println("||||||||||||||||||||||||||||||||testFlatMap|||||||||||||||||||||||||||||");
-	}
+                // 打印每一项
+                .forEach(item -> System.out.println(item));
+        System.out.println("||||||||||||||||||||||||||||||||testFlatMap|||||||||||||||||||||||||||||");
+    }
 
-	private static void testComprehensive() {
-		List<Integer> nums = Lists.newArrayList(1,1,null,2,3,4,null,5,6,7,8,9,10);
-		System.out.println("sum is:"+nums.stream().filter(num -> num != null)
-		                //1,1,2,3,4,5,6,7,8,9,10
-		                //.peek(x -> System.out.println("peek0: "+x))
-		                .distinct()
-		                //1,2,3,4,5,6,7,8,9,10
-		                .mapToInt(num -> num * 2)
-		                //2,4,6,8,10,12
-		                .skip(2)
-		                //6,8,10,12,14,16,18,20
-		                .limit(4)
-		                .peek(System.out::println)
-		                //6,8,10,12
-		                .sum());
-		                //36
-		System.out.println("|||||||||||||||||||||||||||||||||testComprehensive||||||||||||||||||||||||||||");
-	}
-	/**
-	 * 返回一个丢弃原Stream的前N个元素后剩下元素组成的新Stream，如果原Stream中包含的元素个数小于N，那么返回空Stream
-	 */
-	private static void testSkip() {
-		List<Integer> nums = Lists.newArrayList(18,2,3,4,5,6,7,50,100);
-		nums.stream().skip(6).forEach(s->System.out.println(s));
-		System.out.println("|||||||||||||||||||||||||||||||||testSkip||||||||||||||||||||||||||||");
-	}
+    private static void testComprehensive() {
+        List<Integer> nums = Lists.newArrayList(1, 1, null, 2, 3, 4, null, 5, 6, 7, 8, 9, 10);
+        System.out.println("sum is:" + nums.stream().filter(num -> num != null)
+                //1,1,2,3,4,5,6,7,8,9,10
+                //.peek(x -> System.out.println("peek0: "+x))
+                .distinct()
+                //1,2,3,4,5,6,7,8,9,10
+                .mapToInt(num -> num * 2)
+                //2,4,6,8,10,12
+                .skip(2)
+                //6,8,10,12,14,16,18,20
+                .limit(4)
+                .peek(System.out::println)
+                //6,8,10,12
+                .sum());
+        //36
+        System.out.println("|||||||||||||||||||||||||||||||||testComprehensive||||||||||||||||||||||||||||");
+    }
 
-	private static void testLimit() {
-		List<Integer> nums = Lists.newArrayList(18,2,3,4,5,6,7,50,100);
-		nums.stream().limit(6).forEach(System.out::println);
-		System.out.println("|||||||||||||||||||||||||||||||||testLimit||||||||||||||||||||||||||||");
-	}
+    /**
+     * 返回一个丢弃原Stream的前N个元素后剩下元素组成的新Stream，如果原Stream中包含的元素个数小于N，那么返回空Stream
+     */
+    private static void testSkip() {
+        List<Integer> nums = Lists.newArrayList(18, 2, 3, 4, 5, 6, 7, 50, 100);
+        nums.stream().skip(6).forEach(s -> System.out.println(s));
+        System.out.println("|||||||||||||||||||||||||||||||||testSkip||||||||||||||||||||||||||||");
+    }
 
-	private static void testMap() {
-		List<Integer> nums = Lists.newArrayList(50,100);
+    private static void testLimit() {
+        List<Integer> nums = Lists.newArrayList(18, 2, 3, 4, 5, 6, 7, 50, 100);
+        nums.stream().limit(6).forEach(System.out::println);
+        System.out.println("|||||||||||||||||||||||||||||||||testLimit||||||||||||||||||||||||||||");
+    }
+
+    private static void testMap() {
+        List<Integer> nums = Lists.newArrayList(50, 100);
 //		nums.stream().peek(e -> System.out.println(e*100));
-		nums.stream().map(n -> "成绩"+n.toString()).forEach(System.out::println);
-		System.out.println("|||||||||||||||||||||||||||||||||||testMap||||||||||||||||||||||||||");
-	}
+        nums.stream().map(n -> "成绩" + n.toString()).forEach(System.out::println);
+        nums.stream().map(m -> {
+            Map<String, Integer> map = new HashMap<>();
+            map.put("A", m);
+            return map;
+        }).collect(toList()).forEach(r -> System.out.println(r));
+        System.out.println("|||||||||||||||||||||||||||||||||||testMap||||||||||||||||||||||||||");
+    }
 
-	private static void testFilter() {
-		List<Integer> nums = Lists.newArrayList(50,100);
-		nums.stream().filter(s -> s > 90).map(r -> {
-			if(r>90){
-				return "优";
-			} 
-			return "其他";
-		}).forEach(System.out::println);
-		System.out.println("||||||||||||||||||||||||||||||||||||testFilter|||||||||||||||||||||||||");
-	}
+    private static void testFilter() {
+        List<Integer> nums = Lists.newArrayList(50, 100);
+        nums.stream().filter(s -> s > 90).map(r -> {
+            if (r > 90) {
+                return "优";
+            }
+            return "其他";
+        }).collect(Collectors.toList()).forEach(System.out::println);
+        System.out.println("||||||||||||||||||||||||||||||||||||testFilter|||||||||||||||||||||||||");
+    }
 
-	private static void testDistinct() {
-		List<String> names = Lists.newArrayList("A","B","A");
-		names.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
-		System.out.println("||||||||||||||||||||||||||||||||||||testDistinct|||||||||||||||||||||||||");
-	}
+    private static void testDistinct() {
+        List<String> names = Lists.newArrayList("A", "B", "A");
+        names.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
+        System.out.println("||||||||||||||||||||||||||||||||||||testDistinct|||||||||||||||||||||||||");
+    }
 
-	private static void testisPresent() {
-		String str = "JASON";
+    private static void testisPresent() {
+        String str = "JASON";
 //		String str = null;
-		Optional<String> name = Optional.ofNullable(str);
-		System.out.println(name.isPresent());
-		name.ifPresent((value)->{
-			System.out.println("name is : " + value);
-	    });
-		name.orElseGet(() -> "a");
-		System.out.println("|||||||||||||||||||||||||||||||||||||testisPresent||||||||||||||||||||||||");
-	}
-	
-	private static void testLambda() {
-		List<String> names = Lists.newArrayList("A","B");
-		names.stream().map((String n1) -> {return  n1.toLowerCase();}).collect(Collectors.toList());
-		names.stream().map((n2) -> n2.toLowerCase()).collect(Collectors.toList());
-		names.stream().map(String::toLowerCase).collect(Collectors.toList());
-		System.out.println("|||||||||||||||||||||||||||||||||||||testLambda||||||||||||||||||||||||");
-	}
-	
+        Optional<String> name = Optional.ofNullable(str);
+        System.out.println(name.isPresent());
+        name.ifPresent((value) -> {
+            System.out.println("name is : " + value);
+        });
+        name.orElseGet(() -> "a");
+        System.out.println("|||||||||||||||||||||||||||||||||||||testisPresent||||||||||||||||||||||||");
+    }
+
+    private static void testLambda() {
+        List<String> names = Lists.newArrayList("A", "B");
+        names.stream().map((String n1) -> {
+            return n1.toLowerCase();
+        }).collect(Collectors.toList());
+        names.stream().map((n2) -> n2.toLowerCase()).collect(Collectors.toList());
+        names.stream().map(String::toLowerCase).collect(Collectors.toList());
+        System.out.println(Optional.ofNullable(null).orElse(10));
+
+        System.out.println("|||||||||||||||||||||||||||||||||||||testLambda||||||||||||||||||||||||");
+        consumer1.accept("asdf");
+        Supplier<Integer> integerSupplier = () -> new Random().nextInt();
+        integerSupplier.get();
+        
+    }
+
+    public static void printValue(String str) {
+        System.out.println("print value : " + str);
+    }
+
+    public static void printValue(String str1, String str2) {
+        System.out.println("print value : " + str1.concat(str2));
+    }
+    public static Consumer<String> consumer1 = (s) -> System.out.println(s);
 }
